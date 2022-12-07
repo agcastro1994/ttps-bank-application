6ffb1feba71d04267fa83d273c481bb340f67d18
Arturo Castro <agcastro1994@gmail.com>
2022-12-07 01:18:47 -0300

    fix validate messages

--- bank_app/app/models/office.rb
+++ bank_app/app/models/office.rb
@@ -1,7 +1,7 @@
 class Office < ApplicationRecord
     has_many :schedules, dependent: :destroy
 
-    validates :name, presence: true
-    validates :address, presence: true
-    validates :phone, presence: true, length: { minimum: 10 }
+    validates :name, presence: { message: "Disculpe, el nombre de la sucursal no puede estar vacio" }
+    validates :address, presence: { message: "Disculpe, la direccion de la sucursal no puede estar vacia" }
+    validates :phone, presence: true, length: { minimum: 10, message: "Disculpe, el nro de tlf debe tener minimo 10 digitos" }
 end
